756478e0391f9b89ecb421575f09fafbe65a5c27
Anurag <jai.borse01@gmail.com>
2024-04-26 14:56:41 +0530

    Anurag

--- Syrophage/Controllers/HomeController.cs
+++ Syrophage/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace Syrophage.Controllers
             return View();
         }
 
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
