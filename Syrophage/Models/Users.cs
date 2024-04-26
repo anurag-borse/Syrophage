f09ed158cd086bc737f20fd4e7f8a6a12298e682
Anurag <jai.borse01@gmail.com>
2024-04-26 16:45:24 +0530

    Anurag

---
+++ Syrophage/Models/Users.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Syrophage.Models
+{
+    public class Users
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter Name .")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Phone No .")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number should be exactly 10 digits.")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number must be numeric.")]
+
+        public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Email .")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Password .")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$", ErrorMessage = "Password Must be Strong")]
+        public string Password { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+
+        public bool? IsActivated { get; set; }
+    }
+}
--- Syrophage/Program.cs
+++ Syrophage/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Syrophage.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Defaultconnection")));
 
 var app = builder.Build();
 
