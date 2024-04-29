1ce7b786294903b71b97fbdb76cf0e5028b92426
Anurag <jai.borse01@gmail.com>
2024-04-29 15:04:32 +0530

    Dev solved

--- Syrophage/Controllers/LoginController.cs
+++ Syrophage/Controllers/LoginController.cs
@@ -85,11 +85,11 @@ namespace Syrophage.Controllers
 
                 }
             }
-            }
 
             TempData["failed"] = "Login Failed: Invalid Credentials";
             return View();
         }
+
 
 
 
@@ -114,7 +114,7 @@ namespace Syrophage.Controllers
             if (existingEmail != null)
             {
                 TempData["Message"] = "Email is Already Exists";
-                 return RedirectToAction("Register", "Login");
+                return RedirectToAction("Register", "Login");
             }
             // Check if the phone number already exists
             var existingPhoneUser = _db.Users.FirstOrDefault(r => r.Phone == model.Phone);
@@ -123,31 +123,9 @@ namespace Syrophage.Controllers
                 TempData["Message"] = "Phone no. is Already Exists";
                 return RedirectToAction("Register", "Login");
             }
-            if (ModelState.IsValid)
-           
-                //var existingEmailUser = unitOfWorks.Users.GetByEmail(usr.User.Email);
-                var existingEmail = _db.Users.FirstOrDefault(r => r.Email == model.Email);
-                if (existingEmail != null)
-                {
-                    TempData["repeatemail"] = "Email is Already Exists";
-                    // return RedirectToAction("Login", "Login");
-                }
-                // Check if the phone number already exists
-                var existingPhoneUser = _db.Users.FirstOrDefault(r => r.Phone == model.Phone);
-                if (existingPhoneUser != null)
-                {
-                    TempData["repeatephone"] = "Phone no. is Already Exists";
-                    return RedirectToAction("Register", "Login");
-                }
-
-            if (model.Password != model.ConfirmPassword)
-            {
-                TempData["Confirm"] = "The new password and confirmed password do not match.";
-                return View(model);
-            }
 
 
-            if (model!=null)
+            if (model != null)
             {
                 var reg = new Users
                 {
@@ -236,6 +214,7 @@ namespace Syrophage.Controllers
                 _httpContextAccessor.HttpContext.Items["LayoutModel"] = layoutModel;
             }
         }
+
 
 
     }
