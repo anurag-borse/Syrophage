63c0994d924a448dc1360446032859606fe87748
rocky <yadneshchaudhri2323@gmail.com>
2024-05-02 15:15:04 +0530

    Commit All

--- Syrophage/Repository/IRepository/ICategoriesRepository.cs
+++ Syrophage/Repository/IRepository/ICategoriesRepository.cs
@@ -4,6 +4,8 @@ namespace Syrophage.Repository.IRepository
 {
     public interface ICategoriesRepository : IRepository<Categories>
     {
+        Categories GetById(int Id);
 
+        void Update(Categories obj);
     }
 }
